a6aef50c20445194209f8f147006d9371643454c
Francesco Agosti <francesco.agosti93@gmail.com>
2019-04-28 17:16:59 -0700

    Implement redirects and proxy for content

--- proxy/src/handler.ts
+++ proxy/src/handler.ts
@@ -1,22 +1,42 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import proxy  from 'express-http-proxy';
 import isBot from 'isbot';
 
 import { rendertron } from './util/rendertron';
 import logger from './util/logger';
+import { requestTypesToRedirect } from './constants';
+import { urlencoded } from 'body-parser';
+
+const originUrl = 'http://sample-s3-spa.com.s3-website-us-west-2.amazonaws.com';
 
 export const handler = {
-  root: async (req: Request, res: Response): Promise<void> => {
-    if (isBot(req.headers["user-agent"])) {
+  root: async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const url = req.url;
+    if (isBot(req.get('user-agent'))) {
+      logger.info(`Handing bot request for ${url}`);
       return handler.handleBotRequest(req, res);
     }
-    return handler.handleRegularRequest(req, res);
+    logger.info(`Handing regular request for ${url}`);
+    return handler.handleRegularRequest(req, res, next);
   },
   handleBotRequest: async (req: Request, res: Response): Promise<void> => {
     logger.info('Called root endpoint');
-    const response = await rendertron.render('http://sample-s3-spa.com.s3-website-us-west-2.amazonaws.com/');
+    // TODO: Remove base= html tag from rendertron response.
+    const response = await rendertron.render(originUrl);
     res.send(response.data);
   },
-  handleRegularRequest: async (req: Request, res: Response): Promise<void> => {
-    res.redirect('http://sample-s3-spa.com.s3-website-us-west-2.amazonaws.com/');
+  handleRegularRequest: async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // Only proxy text/html content type
+    const proxyHandler = proxy(originUrl);
+    const url = req.url;
+    if (
+      requestTypesToRedirect.reduce((acc, fileType) => acc || url.endsWith(fileType), false)
+    ) {
+      const redirectUrl = `${originUrl}${req.url}`;
+      logger.info(`Redirecting to ${redirectUrl}`);
+      return res.redirect(redirectUrl);
+    }
+    logger.info(`Proxying request for ${req.url} content from ${originUrl}`);
+    return proxyHandler(req, res, next);
   }
 }
--- proxy/src/util/rendertron.ts
+++ proxy/src/util/rendertron.ts
@@ -1,7 +1,5 @@
 import axios, { AxiosAdapter, AxiosResponse } from 'axios';
 
-import logger from '../util/logger';
-
 export const rendertron = {
   render: async (url: string): Promise<AxiosResponse> => {
     const response = await axios.get(`${process.env.RENDERTRON_URL}/render/${url}`);
