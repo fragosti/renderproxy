a54dbb84df29819eb313f3762e44ae72e08925be
Francesco Agosti <francesco.agosti93@gmail.com>
2019-06-05 09:36:31 -0700

    Get dynamic HTTPs almost working with greenlock and greenlock s3 storage

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,5 +1,45 @@
-import Server from './server';
+import Greenlock from 'greenlock-express';
+import GreenlockStorageS3 from 'greenlock-storage-s3';
+import { app } from './app';
 
-const port = parseInt(process.env.PORT, 10);
-export default new Server()
-  .listen(port);
+const tcpPort = parseInt(process.env.PORT, 10);
+
+// const server = createServer(app, port);
+
+const store = GreenlockStorageS3.create({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  bucketRegion: process.env.AWS_S3_BUCKET_REGION,
+  bucketName: process.env.AWS_S3_BUCKET_NAME,
+  configDir: 'acme/',
+  accountsDir: 'accounts/',
+  debug: true,
+});
+
+Greenlock.create({
+  // Let's Encrypt v2 is ACME draft 11
+  version: 'draft-11',
+
+  // server: 'https://acme-v02.api.letsencrypt.org/directory',
+  // Note: If at first you don't succeed, stop and switch to staging
+  server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
+
+  // You MUST change this to a valid email address
+  email: 'agostif93@gmail.com',
+
+  // You MUST NOT build clients that accept the ToS without asking the user
+  agreeTos: true,
+
+  // You MUST change these to valid domains
+  // NOTE: all domains will validated and listed on the certificate
+  approvedDomains: ['samples3spasite.com'],
+
+  store,
+
+  app,
+
+  // Get notified of important updates and help me make greenlock better
+  // communityMember: true
+
+  //, debug: true
+}).listen(tcpPort, 443);
--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -1,30 +1,15 @@
-import bodyParser from 'body-parser';
-import express from 'express';
 import { Application } from 'express';
 import http from 'http';
 import os from 'os';
-import path from 'path';
 
-import { handler } from './handler';
 import l from './util/logger';
 
-const app = express();
-
-export default class ExpressServer {
-  constructor() {
-    const root = path.normalize(__dirname + '/../..');
-    app.set('appPath', root + 'client');
-    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
-    // app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
-    // app.use(cookieParser(process.env.SESSION_SECRET));
-    // app.use(express.static(`${root}/public`));
-    app.get('/*', handler.root);
-  }
-
-  public listen(p: string | number = process.env.PORT): Application {
-    http.createServer(app).listen(p, () => {
-      l.info(`up and running in ${process.env.NODE_ENV || 'development'} @: ${os.hostname() } on port: ${p}}`);
-    });
-    return app;
-  }
-}
+export const createServer = (app: Application, port: number): http.Server => {
+  const httpServer = http.createServer(app).listen(port, () => {
+    l.info(
+      `up and running in ${process.env.NODE_ENV ||
+        'development'} @: ${os.hostname()} on port: ${port}}`,
+    );
+  });
+  return httpServer;
+};
