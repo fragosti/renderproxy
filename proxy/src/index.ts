21316e2fc6b04dd78532cf3abddcd201bcc3350b
Francesco Agosti <francesco.agosti93@gmail.com>
2019-10-06 14:13:55 -0700

    Move unhandled error handlers

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,4 +1,14 @@
 import { app } from './app';
 import { createServer } from './server';
+import { logger } from './util/logger';
 
 createServer(app);
+
+process.on('uncaughtException', (err) => {
+  logger.error(err);
+  process.exit(1);
+});
+process.on('unhandledRejection', (err) => {
+  logger.error(err);
+  process.exit(1);
+});
